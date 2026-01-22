e165cd7ef96442f4f8555bd2bc6e8c5e3c09c404
rustamovjoxon80 <rustamovjoxon80@gmail.com>
2026-01-22 13:43:37 +0500

    tayyor

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/Navbar'
+import Section3 from './components/Section3'
 
 function App() {
 
   return (
     <>
-    <Navbar/>
-      
+    {/* <Navbar/>? */}
+    <Section3/>
     </>
   )
 }
